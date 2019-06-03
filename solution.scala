faa6a51f2b45721655b70a05b59673ff13a67612
Konstantin Nosikov <Konstantin.Nosikov@emc.com>
2019-06-03 09:31:18 +0300

    Small fixes and format

--- solution.scala
+++ solution.scala
@@ -1,21 +1,9 @@
-val file = sc.textFile("hdfs://<IP>/nasa/jul95")
+val file = sc.textFile("hdfs://<IP>:9000/nasa/jul95")
 
 /*
-QUESTIONS:
-
-1. Подготовить список запросов, которые закончились 5xx ошибкой, с количеством неудачных запросов
-	Какие пары? (ошибка -> количество), "METHOD ADDRESS" -> количество?)
-	one = rdd.dropFailed().toColumns().map(line => (line, code))
-	two = one.map((line, code) => (request, 1)).reduceByKey((a, b) => a + b)
-
-2. Подготовить временной ряд с количеством запросов по датам для всех используемых комбинаций http методов и return codes. Исключить из результирующего файла комбинации, где количество событий в сумме было меньше 10.
-	one = rdd.dropFailed().toColumns().map(line => ((date, method, code), 1)).reduceByKey((a, b) => a + b).sortBy(a => a._0._0)
-	two = one.filter(a => a._1 >= 10)
-
-3. Произвести расчет скользящим окном в одну неделю количества запросов закончившихся с кодами 4xx и 5xx (?)
+TASK 1
+Подготовить список запросов, которые закончились 5xx ошибкой, с количеством неудачных запросов
 */
-
-// TASK 1
 val myLogRegex = raw"""^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(.+)" (\d{3}) (\S+)""".r
 file.
 	filter(line => line match {
@@ -31,7 +19,11 @@ rdd.
 	foreach(pair => println(pair._2 + ": " + pair._1))
 
 
-// TASK 2.
+/*
+TASK 2
+Подготовить временной ряд с количеством запросов по датам для всех используемых комбинаций http методов и return codes.
+Исключить из результирующего файла комбинации, где количество событий в сумме было меньше 10.
+*/
 val myLogRegex = raw"""^(\S+) (\S+) (\S+) \[([\w/]+):([\w:]+\s[+\-]\d{4})\] "(\S+) (.*)" (\d{3}) (\S+)""".r
 val rdd = file.
 	filter(line => line match {
@@ -43,14 +35,17 @@ val rdd = file.
 		case _ => (("01/Jan/1960", "UNKNOWN", -1), 1)
 	}).
 	reduceByKey((a, b) => a + b).
-	sortBy(a => a._1._1).
-	filter(a => a._2 > 10)
+	filter(a => a._2 > 9).
+	sortBy(a => a._1._1)
 rdd.
 	collect().
 	foreach(pair => println(pair._1 + ": " + pair._2))
 
 
-// TASK 3
+/*
+TASK 3
+Произвести расчет скользящим окном в одну неделю количества запросов закончившихся с кодами 4xx и 5xx
+*/
 val logRegex = raw"""^(\S+) (\S+) (\S+) \[([\w:/]+\s[+\-]\d{4})\] "(.+)" (\d{3}) (\S+)""".r
 val myLogRegex = raw"""^(\S+) (\S+) (\S+) \[([\w/]+):([\w:]+\s[+\-]\d{4})\] "(\S+) (.*)" (\d{3}) (\S+)""".r
 val days = (8 to 31).toArray
@@ -64,8 +59,11 @@ val rdd = file.
 		case _ => ("01/Jan/1960", 1)
 	}).
 	reduceByKey((a, b) => a + b).
+	// Cartesian with RDD [8, 9, ..., 31]
 	cartesian(sc.parallelize(days)).
+	// Filter ((date, n), day) to satisfy "Day of date is less than 7 days lower than 'day'"
 	filter(a => a._1._1.split("/")(0).toInt <= a._2 && a._1._1.split("/")(0).toInt > (a._2 - 7)).
+	// Turn ((date, n), day) to (day-7 - day, n)
 	map(a => ((a._2 - 7) + "-" + (a._2), a._1._2)).
 	reduceByKey((a, b) => a + b).
 	sortBy(a => a._1.split("-")(0).toInt)
@@ -73,14 +71,13 @@ rdd.
 	collect().
 	foreach(pair => println(pair._1 + ":\t" + pair._2))
 
-=== OR ===
+/* === OR === */
 
 import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType, DateType};
 import java.text.SimpleDateFormat
 import org.apache.spark.sql.Row
 val oldFormat = new SimpleDateFormat("dd/MMM/yyyy")
 val newFormat = new SimpleDateFormat("yyyy-MM-dd")
-val timestampFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S")
 def dfSchema(columnNames: List[String]): StructType =
 	StructType(
 		Seq(
@@ -97,7 +94,7 @@ val rdd = file.
 	map(line => line match {
 		case myLogRegex(host, client_id, user_id, date, time, method, request, code, size) => 
 			Row(newFormat.format(oldFormat.parse(date)), 1)
-		case _ => Row("01/Jan/1960", 1)
+		case _ => Row("1960-01-01", 1)
 	})
 val df = spark.
 	createDataFrame(rdd, schema).
